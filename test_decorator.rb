ad708ed02b456164331f771195c9275116d77637
Kelvin-Ben <[kevoben14@gmail.com]>
2022-12-22 01:27:41 +0300

    Test decorator actions

---
+++ test_decorator.rb
@@ -0,0 +1,10 @@
+require 'trimmer'
+require 'person'
+require 'capitalize'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+capitalized_trimmed_person.correct_name
